c0bf345a8435c7a53c0fca501d12a9b034e35aab
dajun <rcdchen@qq.com>
2019-08-16 18:31:37 +0800

    增强counsel-grep-or-swiper并绑定到C-s，超爽！可自动搜索光标下的单词或选中的

--- config.el
+++ config.el
@@ -1,7 +1,7 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-
+;; ======= 显示 display  =========================================================
 ;; 使用 monokai 主题。 要在packages.el中安装  monokai-theme
 (require 'monokai-theme)
 (setq doom-theme 'monokai)
@@ -10,6 +10,7 @@
 (setq evil-visual-state-cursor 'hbar)  ;; 默认是 hollow空心块， 会造成在手动选中高亮时，最后一个字符看不见，这里改成 hbar下划线。
 
 ;; 把背景色改成 jonathan blow 使用的绿色背景: #0B2B2D
+;; 可通过 M-x list-faces-display 来查看和显示有关的配置信息。
 (custom-set-faces
  '(default ((t (:background "#0B2B2D"))))
 
@@ -25,9 +26,20 @@
  '(region ((t (:background "#FF9933" :foreground "black"))))  ;; 手动选中高亮颜色
  '(evil-ex-lazy-highlight ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
  '(iedit-occurrence ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
+
+ '(swiper-background-match-face-1 ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
+ '(swiper-background-match-face-2 ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
+ '(swiper-background-match-face-3 ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
+ '(swiper-background-match-face-4 ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
+ '(swiper-match-face-1 ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
+ '(swiper-match-face-2 ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
+ '(swiper-match-face-3 ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
+ '(swiper-match-face-4 ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
  )
 
 
+;; ======= 编辑   =========================================================
+;;
 ;;调整缩进、格式： 系统默认也是keymap到 C-M-\, 不过只是对选中区域有效，这里让当前激活的buffer也可以。
 (defun dj@indent-buffer()
   (interactive)
@@ -54,6 +66,41 @@
   )
 
 
+;; ======= 搜索匹配    =========================================================
+;;
+;; *：evil normal模式  *  自动匹配光标下的单词，高亮显示， 可用n匹配下项。
+;; / 绑定到evil-ex-search-forward：  用于需要手动输入的时候。
+;; SPC / b 绑定到 counsel-grep-or-swiper ： 用于手动输入， 下面会有所有匹配结果的汇总。
+;; C-s 默认绑定到 isearch-forward: 不好用， swiper-isearch or swiper-isearch-thing-at-point 感觉没counsel-grep-or-swiper好用。
+(defun dj@counsel-grep-or-swiper-at-point () ; 增强counsel-grep-or-swiper，默认可以自动搜索光标下的单词、或选中的。
+  "C-s 默认绑定到 isearch-forward: 不好用。通过增强counsel-grep-or-swiper，可以自动搜索光标下的单词、或选中的。"
+  (interactive)
+  (if (window-minibuffer-p)
+      (let (bnd str regionp)
+        (with-ivy-window
+          (setq bnd
+                (if (setq regionp (region-active-p))
+                    (prog1 (cons (region-beginning) (region-end))
+                      (deactivate-mark))
+                  (bounds-of-thing-at-point 'symbol)))
+          (setq str (buffer-substring-no-properties (car bnd) (cdr bnd))))
+        (insert str)
+        (unless regionp
+          (ivy--insert-symbol-boundaries)))
+    (let (thing)
+      (if (use-region-p)
+          (progn
+            (setq thing (buffer-substring-no-properties
+                         (region-beginning) (region-end)))
+            (goto-char (region-beginning))
+            (deactivate-mark))
+        (let ((bnd (bounds-of-thing-at-point 'symbol)))
+          (when bnd
+            (goto-char (car bnd)))
+          (setq thing (ivy-thing-at-point))))
+      (counsel-grep-or-swiper thing))))
+(global-set-key (kbd "C-s") 'dj@counsel-grep-or-swiper-at-point)
+
 ;;配置 Occur Mode 使其默认搜索当前被选中的或者在光标下的字符串, M-s o 默认就有，这里只是增强：默认可以搜索广标下的字符。
 (defun occur-dwim ()
   "Call `occur' with a sane default."
@@ -71,8 +118,11 @@
 
 
 
+;; ======= keymap    =========================================================
+;;
 (map!
  :nvi "C-k" #'kill-line                 ; 在正常、可视、插入模式下, C-k 删除当前行光标后的内容。
  :nvi "C-e" #'doom/forward-to-last-non-comment-or-eol ; 在正常、可视、插入模式下, C-e 去到行尾。
-  :nvi "M-;" #'comment-dwim     ; 在正常、可视、插入模式下, M-; 在行尾添加注释。s-/  是对整行或选中区域进行注释或取消注释。
+ :nvi "M-;" #'comment-dwim     ; 在正常、可视、插入模式下, M-; 在行尾添加注释。s-/  是对整行或选中区域进行注释或取消注释。
+
  )
