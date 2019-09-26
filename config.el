6184efa2a7864ce78099443f263d3b9f351fb25b
dajun <rcdchen@qq.com>
2019-09-26 16:25:43 +0800

    doom 改用 *straight* 代替 *package*

--- config.el
+++ config.el
@@ -1,31 +1,31 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;; ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
-;; ======= 显示 display  =========================================================
-;;
+;; ;; Place your private configuration here
+;; ;; ======= 显示 display  =========================================================
+;; ;;
 (use-package monokai-theme              ;; 使用 monokai 主题。 要在packages.el中安装  monokai-theme
   :config
   (setq doom-theme 'monokai
         monokai-highlight-line "black"    ; 当前行高亮背景色
         ))
-(setq doom-font (font-spec :family "Source Code Pro" :size 15))  ;; 使用Source Code Pro 字体 大小 15
+(setq doom-font (font-spec :family "Source Code Pro" :size 16))  ;; 使用Source Code Pro 字体 大小 15
 (setq initial-frame-alist (quote ((fullscreen . maximized))))   ;; 默认全屏
 (setq evil-visual-state-cursor 'hbar)  ;; 默认是 hollow空心块， 会造成在手动选中高亮时，最后一个字符看不见，这里改成 hbar下划线。
 
 
-;; 把背景色改成 jonathan blow 使用的绿色背景: #0B2B2D
-;; 可通过 M-x list-faces-display 来查看和显示有关的配置信息。
+;; ;; 把背景色改成 jonathan blow 使用的绿色背景: #0B2B2D
+;; ;; 可通过 M-x list-faces-display 来查看和显示有关的配置信息。
 (custom-set-faces
  '(default ((t (:background "#0B2B2D"))))
 
- '(solaire-default-face ((t (:background "#0B2B2D"))))
- '(solaire-fringe-face ((t (:background "#0B2B2D"))))
- '(solaire-hl-line-face ((t (:background "#0B2B2D"))))
- '(solaire-line-number-face ((t (:background "#0B2B2D"))))
- '(solaire-minibuffer-face ((t (:background "#0B2B2D"))))
- '(solaire-mode-line-face ((t (:background "#0B2B2D"))))
- '(solaire-mode-line-inactive-face ((t (:background "#0B2B2D"))))
- '(solaire-org-hide-face ((t (:background "#0B2B2D"))))
+ ;;  '(solaire-default-face ((t (:background "#0B2B2D"))))
+ ;;  '(solaire-fringe-face ((t (:background "#0B2B2D"))))
+ ;;  '(solaire-hl-line-face ((t (:background "#0B2B2D"))))
+ ;;  '(solaire-line-number-face ((t (:background "#0B2B2D"))))
+ ;;  '(solaire-minibuffer-face ((t (:background "#0B2B2D"))))
+ ;;  '(solaire-mode-line-face ((t (:background "#0B2B2D"))))
+ ;;  '(solaire-mode-line-inactive-face ((t (:background "#0B2B2D"))))
+ ;;  '(solaire-org-hide-face ((t (:background "#0B2B2D"))))
 
  '(region ((t (:background "#FF9933" :foreground "black"))))  ;; 手动选中高亮颜色
  '(evil-ex-lazy-highlight ((t (:background "#FF9933" :foreground "black"))))  ;; /搜索匹配高亮颜色
@@ -42,20 +42,20 @@
  )
 
 
-(use-package golden-ratio   ; 自动黄金分割窗口大小。
-  :config
-  (golden-ratio-mode 1)
-  )
+;; (use-package golden-ratio   ; 自动黄金分割窗口大小。
+;;   :config
+;;   (golden-ratio-mode 1)
+;;   )
 
 (use-package centered-cursor-mode       ; 设置默认当前行居中
   :config
   (global-centered-cursor-mode)
   )
-;; ======= 编辑   =========================================================
-;;
-;; evil-multiedit 方式: 使用 v 选中文本后，键入 R 可以进入多光标编辑模式. 这时使用 C-n C-p 可以在匹配文本中轮换，按 RET 反选当前匹配项.
-;;
-;;调整缩进、格式： 系统默认也是keymap到 C-M-\, 不过只是对选中区域有效，这里让当前激活的buffer也可以。
+;; ;; ======= 编辑   =========================================================
+;; ;;
+;; ;; evil-multiedit 方式: 使用 v 选中文本后，键入 R 可以进入多光标编辑模式. 这时使用 C-n C-p 可以在匹配文本中轮换，按 RET 反选当前匹配项.
+;; ;;
+;; ;;调整缩进、格式： 系统默认也是keymap到 C-M-\, 不过只是对选中区域有效，这里让当前激活的buffer也可以。
 (defun dj@indent-buffer()
   (interactive)
   (indent-region (point-min) (point-max)))
@@ -72,7 +72,7 @@
 (global-set-key (kbd "C-M-\\") 'dj@indent-region-or-buffer)
 
 
-;;光标在括号内时就高亮包含内容的两个括号
+;; ;;光标在括号内时就高亮包含内容的两个括号
 (defadvice show-paren-function (around fix-show-paren-function activate)
   (cond ((looking-at-p "\\s(") ad-do-it)
         (t (save-excursion
@@ -81,33 +81,32 @@
   )
 
 
-;; 设置默认缩进 4 个空格， tab也是4个空格
-(setq-default c-basic-offset 4
-              tab-width 4
-              indent-tabs-mode nil      ; t 使用 TAB 作格式化字符  nil 使用空格代替tab作格式化字符
-              tab-always-indent t )
-(setq c-default-style "linux")
-(add-hook 'c-mode-common-hook   ; 强制c/c++ 使用 4个空格缩进, 如果只是c++，可以用c++-mode-hook
-          '(lambda()
-             (setq c-basic-offset 4)
-             (c-set-offset 'substatement-open 0)
-             ))
+;; ;; 设置默认缩进 4 个空格， tab也是4个空格
+;; (setq-default c-basic-offset 4
+;;               tab-width 4
+;;               indent-tabs-mode nil      ; t 使用 TAB 作格式化字符  nil 使用空格代替tab作格式化字符
+;;               tab-always-indent t )
+;; (setq c-default-style "linux")
+;; (add-hook 'c-mode-common-hook   ; 强制c/c++ 使用 4个空格缩进, 如果只是c++，可以用c++-mode-hook
+;;           '(lambda()
+;;              (setq c-basic-offset 4)
+;;              (c-set-offset 'substatement-open 0)
+;;              ))
+
+
+;; ;; 在org，中英混排出现空格时，禁止自动折行生成真正的新行。
+;; (remove-hook 'org-mode-hook #'auto-fill-mode)
 
 
 
-;; 在org，中英混排出现空格时，禁止自动折行生成真正的新行。
-(remove-hook 'org-mode-hook #'auto-fill-mode)
-
-
-
-;; ======= 搜索匹配    =========================================================
-;;
-;; *：evil normal模式  *  自动匹配光标下的单词，高亮显示， 可用n匹配下项。
-;; zt zz zb： evil nomal 模式下，把当前行移动到屏幕顶部、总部、底部。
-;; H  L:evil nomal 模式下，把光标移动到屏幕顶部， 底部。 gg是移动到到第一行， G是移动到最后一行。
-;; / 绑定到evil-ex-search-forward：  用于需要手动输入的时候。
-;; SPC / b 绑定到 counsel-grep-or-swiper ： 用于手动输入， 下面会有所有匹配结果的汇总。
-;; C-s 默认绑定到 isearch-forward: 不好用， swiper-isearch or swiper-isearch-thing-at-point 感觉没counsel-grep-or-swiper好用。
+;; ;; ======= 搜索匹配    =========================================================
+;; ;;
+;; ;; *：evil normal模式  *  自动匹配光标下的单词，高亮显示， 可用n匹配下项。
+;; ;; zt zz zb： evil nomal 模式下，把当前行移动到屏幕顶部、总部、底部。
+;; ;; H  L:evil nomal 模式下，把光标移动到屏幕顶部， 底部。 gg是移动到到第一行， G是移动到最后一行。
+;; ;; / 绑定到evil-ex-search-forward：  用于需要手动输入的时候。
+;; ;; SPC / b 绑定到 counsel-grep-or-swiper ： 用于手动输入， 下面会有所有匹配结果的汇总。
+;; ;; C-s 默认绑定到 isearch-forward: 不好用， swiper-isearch or swiper-isearch-thing-at-point 感觉没counsel-grep-or-swiper好用。
 (defun dj@counsel-grep-or-swiper-at-point () ; 增强counsel-grep-or-swiper，默认可以自动搜索光标下的单词、或选中的。
   "C-s 默认绑定到 isearch-forward: 不好用。通过增强counsel-grep-or-swiper，可以自动搜索光标下的单词、或选中的。"
   (interactive)
@@ -137,29 +136,29 @@
       (counsel-grep-or-swiper thing))))
 (global-set-key (kbd "C-s") 'dj@counsel-grep-or-swiper-at-point)
 
-;;配置 Occur Mode 使其默认搜索当前被选中的或者在光标下的字符串, M-s o 默认就有，这里只是增强：默认可以搜索广标下的字符。
-(defun occur-dwim ()
-  "Call `occur' with a sane default."
-  (interactive)
-  (push (if (region-active-p)
-            (buffer-substring-no-properties
-             (region-beginning)
-             (region-end))
-          (let ((sym (thing-at-point 'symbol)))
-            (when (stringp sym)
-              (regexp-quote sym))))
-        regexp-history)
-  (call-interactively 'occur))
-(global-set-key (kbd "M-s o") 'occur-dwim)
+;; ;;配置 Occur Mode 使其默认搜索当前被选中的或者在光标下的字符串, M-s o 默认就有，这里只是增强：默认可以搜索广标下的字符。
+;; (defun occur-dwim ()
+;;   "Call `occur' with a sane default."
+;;   (interactive)
+;;   (push (if (region-active-p)
+;;             (buffer-substring-no-properties
+;;              (region-beginning)
+;;              (region-end))
+;;           (let ((sym (thing-at-point 'symbol)))
+;;             (when (stringp sym)
+;;               (regexp-quote sym))))
+;;         regexp-history)
+;;   (call-interactively 'occur))
+;; (global-set-key (kbd "M-s o") 'occur-dwim)
 
 
 
-;; ======= keymap    =========================================================
-;;
+;; ;; ======= keymap    =========================================================
+;; ;;
 (map!
  :nvi "C-k" #'kill-line                 ; 在正常、可视、插入模式下, C-k 删除当前行光标后的内容。
  :nvi "C-e" #'doom/forward-to-last-non-comment-or-eol ; 在正常、可视、插入模式下, C-e 去到行尾。
- :nvi "M-;" #'comment-dwim     ; 在正常、可视、插入模式下, M-; 在行尾添加注释。s-/  是对整行或选中区域进行注释或取消注释。
+  :nvi "M-;" #'comment-dwim     ; 在正常、可视、插入模式下, M-; 在行尾添加注释。s-/  是对整行或选中区域进行注释或取消注释。
  :nvi "s-/" #'comment-line     ; S代表Shift键盘；s代表Command键，默认s-/绑定到evil-commentary-line， 感觉没comment-line好用
 
  )
@@ -167,7 +166,7 @@
 (map! :leader
       (:prefix ("t" . "toggle")                               ;  SPC t 切换 toggle
         :desc "当前行居中"                  "-" #'centered-cursor-mode ; 切换当前行居中
-        :desc "自动调整窗口大小"             "w" #'golden-ratio-mode; 切换自动调整当前窗口大小
-        :desc "自动折行"                   "v" #'visual-line-mode	; 自动折行，虚拟成n行，上下行、头尾移动方便。 toggle-truncate-lines自动折行，但逻辑上还是一行，不好用。
-        ;; :desc "org显示内嵌的图片"       "m" #'org-toggle-inline-images ; 在org-mode下， 这个函数系统已经默认绑定到 z i
+;;         :desc "自动调整窗口大小"             "w" #'golden-ratio-mode; 切换自动调整当前窗口大小
+;;         :desc "自动折行"                   "v" #'visual-line-mode	; 系统提供了word-wrap模块，启用后，可通过 SPC t w 切换。自动折行，虚拟成n行，上下行、头尾移动方便。 toggle-truncate-lines自动折行，但逻辑上还是一行，不好用。
+;;         ;; :desc "org显示内嵌的图片"       "m" #'org-toggle-inline-images ; 在org-mode下， 这个函数系统已经默认绑定到 z i
         ))
--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-;; Copy this file to ~/.doom.d/init.el or ~/.config/doom/init.el ('doom
-;; quickstart' will do this for you). The `doom!' block below controls what
-;; modules are enabled and in what order they will be loaded. Remember to run
-;; 'doom refresh' after modifying it.
+;; Copy this file to ~/.doom.d/init.el or ~/.config/doom/init.el ('doom install'
+;; will do this for you). The `doom!' block below controls what modules are
+;; enabled and in what order they will be loaded. Remember to run 'doom refresh'
+;; after modifying it.
 ;;
 ;; More information about these modules (and what flags they support) can be
 ;; found in modules/README.org.
@@ -24,17 +24,18 @@
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
-       hl-todo           ; highlight TODO/FIXME/NOTE tags
-       ;;indent-guides     ; highlighted indent columns
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       ;;hydra
+       indent-guides     ; highlighted indent columns
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink the current line after jumping
-       ;;neotree           ; a project drawer, like NERDTree for vim
+       neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
-       ;;pretty-code       ; replace bits of code with pretty symbols
-       ;;tabbar            ; FIXME an (incomplete) tab bar for Emacs
+       ;; pretty-code       ; replace bits of code with pretty symbols
+       tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
@@ -46,19 +47,17 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       ;; (format +onsave)  ; automated prettiness
        ;;lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
+       parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
+       word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired            ; making dired pretty [functional]
-				;;+ranger         ; bringing the goodness of ranger to dired
-				+icons          ; colorful icons for dired-mode
-        )
+       dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        vc                ; version-control and Emacs, sitting in a tree
 
@@ -73,7 +72,7 @@
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
-       ;; editorconfig      ; let someone else argue about tabs vs spaces
+       ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        eval              ; run code, run (also, repls)
        flycheck          ; tasing you for every semicolon you forget
@@ -81,8 +80,8 @@
        ;;gist              ; interacting with github gists
        (lookup           ; helps you navigate your code and documentation
         +docsets)        ; ...or in Dash docsets locally
-       ;;lsp
-       ;;macos             ; MacOS-specific commands
+       lsp
+       macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -119,6 +118,7 @@
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
+       ;;lean
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
@@ -126,10 +126,10 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
-        +dragndrop       ; file drag & drop support
-        +ipython         ; ipython support for babel
-        +pandoc          ; pandoc integration into org's exporter
-        +present)        ; using Emacs for presentations
+        +dragndrop       ; drag & drop files/images into org buffers
+        +ipython         ; ipython/jupyter support for babel
+        +pandoc          ; export-with-pandoc support
+        +present)        ; using org-mode for presentations
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
@@ -159,16 +159,12 @@
        ;; should be loaded late.
        :app
        ;;calendar
-       ;;irc              ; how neckbeards socialize
+       ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
-       ;;(write            ; emacs as a word processor (latex + org + markdown)
+       ;;(write            ; emacs for writers (fiction, notes, papers, etc.)
        ;; +wordnut         ; wordnet (wn) search
        ;; +langtool)       ; a proofreader (grammar/style check) for Emacs
-
-       :collab
-       ;;floobits          ; peer programming for a price
-       ;;impatient-mode    ; show off code over HTTP
 
        :config
        ;; For literate config users. This will tangle+compile a config.org
@@ -179,4 +175,3 @@
        ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
        ;; config. Use it as a reference for your own modules.
        (default +bindings +smartparens))
-
