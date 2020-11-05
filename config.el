1bac44ce40a51f0fb81fbd638bde4c645cfcc175
dajun <rcdchen@qq.com>
2020-11-05 17:15:51 +0800

    sis新版本配置调整

--- config.el
+++ config.el
@@ -190,15 +190,20 @@
 
 
 (use-package! sis   ; 原生输入法切换：smart-input-source ，配合evil使用更佳: 汉字后面跟空格触发 inline english 区域; 光标离开区域，或者回车，关闭 inline english 区域。
-  ;; :hook
+;; :hook
   ;; enable the /follow context/ and /inline region/ mode for specific buffers
-  ;; (((text-mode prog-mode) . sis-follow-context-mode)
+  ;; (((text-mode prog-mode) . sis-context-mode)
   ;;  ((text-mode prog-mode) . sis-inline-mode))
 
   :config
+  ;; For MacOS
   (sis-ism-lazyman-config
+  
+   ;; English input source may be: "ABC", "US" or another one.
    ;; "com.apple.keylayout.ABC"
    "com.apple.keylayout.US"
+
+   ;; Other language input source: "rime", "sogou" or another one.
    ;; "im.rime.inputmethod.Squirrel.Rime"
    "com.sogou.inputmethod.sogou.pinyin")
 
@@ -206,8 +211,8 @@
   (sis-global-cursor-color-mode t)
   ;; enable the /respect/ mode
   (sis-global-respect-mode t)
-  ;; enable the /follow context/ mode for all buffers
-  (sis-global-follow-context-mode t)
+  ;; enable the /context/ mode for all buffers
+  (sis-global-context-mode t)
   ;; enable the /inline english/ mode for all buffers
   (sis-global-inline-mode t)
   )
